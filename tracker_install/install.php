f620d21c663c601f33f5052c742e1360bb396a4e
jrsweets <jrsweets@e70539a1-e9aa-4707-9e48-a9b25a7622e0>
2008-03-12 16:19:02 +0000

    Move attachments to files/tracker Update installation script to move files
    git-svn-id: https://subversion.assembla.com/svn/phpBB_Tracker/trunk@56 e70539a1-e9aa-4707-9e48-a9b25a7622e0

--- tracker_install/install.php
+++ tracker_install/install.php
@@ -151,6 +151,14 @@ switch ($mode)
 						WHERE project_id = ' . (int) $item['project_id'];
 						$db->sql_query($sql);
 					}
+					
+					if ($tracker->config['attachment_path'] == 'includes/tracker/files')
+					{
+						echo '<br /><h1>Moving attachments to new directory</h1>';
+						dircopy('./../includes/tracker/files', './../files/tracker', true, false);
+						remove_dir('./../includes/tracker/files');
+						$tracker->set_config('attachment_path', 'files/tracker');
+					}
 
 				break;
 
@@ -685,6 +693,154 @@ class install_mod
 	function format_version($version)
 	{
 		return str_replace(' ', '.', $version);
+	}
+}
+
+/* Copies a dir to another. Optionally caching the dir/file structure, used to synchronize similar destination dir (web farm).
+ * mzheng at [s-p-a-m dot ]procuri dot com
+ * @param $src_dir str Source directory to copy. 
+ * @param $dst_dir str Destination directory to copy to. 
+ * @param $verbose bool Show or hide file copied messages
+ * @param $use_cached_dir_trees bool Set to true to cache src/dst dir/file structure. Used to sync to web farms
+ *                     (avoids loading the same dir tree in web farms; making sync much faster). 
+ * @return Number of files copied/updated.
+ * @example 
+ *     To copy a dir: 
+ *         dircopy("c:\max\pics", "d:\backups\max\pics"); 
+ *
+ *     To sync to web farms (webfarm 2 to 4 must have same dir/file structure (run once with cache off to make sure if necessary)): 
+ *        dircopy("//webfarm1/wwwroot", "//webfarm2/wwwroot", false, true); 
+ *        dircopy("//webfarm1/wwwroot", "//webfarm3/wwwroot", false, true); 
+ *        dircopy("//webfarm1/wwwroot", "//webfarm4/wwwroot", false, true); 
+ */
+function dircopy($src_dir, $dst_dir, $verbose = false, $use_cached_dir_trees = false) 
+{    
+	static $cached_src_dir;
+	static $src_tree; 
+	static $dst_tree;
+	$num = 0;
+
+	if (($slash = substr($src_dir, -1)) == "\\" || $slash == "/") 
+	{
+		$src_dir = substr($src_dir, 0, strlen($src_dir) - 1); 
+	}
+	
+	if (($slash = substr($dst_dir, -1)) == "\\" || $slash == "/") 
+	{
+		$dst_dir = substr($dst_dir, 0, strlen($dst_dir) - 1);  
+	}
+
+	if (!$use_cached_dir_trees || !isset($src_tree) || $cached_src_dir != $src_dir)
+	{
+		$src_tree = get_dir_tree($src_dir);
+		$cached_src_dir = $src_dir;
+		$src_changed = true;  
+	}
+	
+	if (!$use_cached_dir_trees || !isset($dst_tree) || $src_changed)
+	{
+		$dst_tree = get_dir_tree($dst_dir);
+	}
+	
+	if (!is_dir($dst_dir)) 
+	{
+		mkdir($dst_dir, 0777, true);  
+	}
+	
+	foreach ($src_tree as $file => $src_mtime) 
+	{
+		if (!isset($dst_tree[$file]) && $src_mtime === false) // dir
+		{
+			mkdir("$dst_dir/$file"); 
+		}
+		elseif (!isset($dst_tree[$file]) && $src_mtime || isset($dst_tree[$file]) && $src_mtime > $dst_tree[$file])  // file
+		{
+			if (copy("$src_dir/$file", "$dst_dir/$file")) 
+			{
+				if ($verbose) 
+				{
+					echo "Copied '$src_dir/$file' to '$dst_dir/$file'<br>\r\n";
+				}
+				touch("$dst_dir/$file", $src_mtime); 
+				$num++; 
+			} 
+			else 
+			{
+				echo "<font color='red'>File '$src_dir/$file' could not be copied!</font><br>\r\n";
+			}
+		}        
+	}
+
+	return $num; 
+}
+
+/* Creates a directory / file tree of a given root directory
+ * mzheng at [s-p-a-m dot ]procuri dot com
+ * @param $dir str Directory or file without ending slash
+ * @param $root bool Must be set to true on initial call to create new tree. 
+ * @return Directory & file in an associative array with file modified time as value. 
+ */
+function get_dir_tree($dir, $root = true) 
+{
+	static $tree;
+	static $base_dir_length; 
+
+	if ($root)
+	{ 
+		$tree = array();  
+		$base_dir_length = strlen($dir) + 1;  
+	}
+
+	if (is_file($dir)) 
+	{
+		//if (substr($dir, -8) != "/CVS/Tag" && substr($dir, -9) != "/CVS/Root"  && substr($dir, -12) != "/CVS/Entries")
+		$tree[substr($dir, $base_dir_length)] = filemtime($dir); 
+	} 
+	elseif (is_dir($dir) && $di = dir($dir)) // add after is_dir condition to ignore CVS folders: && substr($dir, -4) != "/CVS"
+	{
+		if (!$root) 
+		{
+			$tree[substr($dir, $base_dir_length)] = false;  
+		}
+		
+		while (($file = $di->read()) !== false)
+		{
+			if ($file != "." && $file != "..")
+			{
+				get_dir_tree("$dir/$file", false);  
+			}
+		}
+		$di->close(); 
+	}
+
+	if ($root)
+	{
+		return $tree;     
+	}
+}
+
+function remove_dir($dir)
+{
+	if(is_dir($dir))
+	{
+		$dir = (substr($dir, -1) != "/")? $dir."/":$dir;
+		$openDir = opendir($dir);
+		while($file = readdir($openDir))
+		{
+			if(!in_array($file, array(".", "..")))
+			{
+				if(!is_dir($dir.$file))
+				{
+					@unlink($dir.$file);
+				}
+				else
+				{
+					remove_dir($dir.$file);
+				}
+			}
+		}
+		closedir($openDir);
+		@rmdir($dir);
 	}
 }
 
