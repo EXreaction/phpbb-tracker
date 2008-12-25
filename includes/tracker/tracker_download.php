19546509f678a1d72478dc20019ea8d86f40bf22
JRSweets <JRSweets@e70539a1-e9aa-4707-9e48-a9b25a7622e0>
2008-12-25 18:31:16 +0000

    Ok this should really fix the downloads :)
    git-svn-id: https://subversion.assembla.com/svn/phpBB_Tracker/trunk@181 e70539a1-e9aa-4707-9e48-a9b25a7622e0

--- includes/tracker/tracker_download.php
+++ includes/tracker/tracker_download.php
@@ -58,7 +58,7 @@ if (!$auth->acl_get('u_tracker_download'))
 	trigger_error('SORRY_AUTH_VIEW_ATTACH');
 }
 
-$sql = 'SELECT attach_id, ticket_id, post_id, extension, is_orphan, poster_id
+$sql = 'SELECT attach_id, ticket_id, post_id, extension, is_orphan, poster_id, filetime
 	FROM ' . TRACKER_ATTACHMENTS_TABLE . "
 	WHERE attach_id = $download_id";
 $result = $db->sql_query_limit($sql, 1);
@@ -91,7 +91,7 @@ if (!download_allowed())
 $download_mode = (int) $tracker->api->extensions[$attachment['extension']]['download_mode'];
 
 // Fetching filename here to prevent sniffing of filename
-$sql = 'SELECT attach_id, is_orphan, ticket_id, post_id, extension, physical_filename, real_filename, mimetype
+$sql = 'SELECT attach_id, is_orphan, ticket_id, post_id, extension, physical_filename, real_filename, mimetype, filetime
 	FROM ' . TRACKER_ATTACHMENTS_TABLE . "
 	WHERE attach_id = $download_id";
 $result = $db->sql_query_limit($sql, 1);
@@ -148,18 +148,18 @@ else
 */
 function wrap_img_in_html($src, $title)
 {
-        echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-Strict.dtd">';
-        echo '<html>';
-        echo '<head>';
-        echo '<meta http-equiv="content-type" content="text/html; charset=UTF-8" />';
-        echo '<title>' . $title . '</title>';
-        echo '</head>';
-        echo '<body>';
-        echo '<div>';
-        echo '<img src="' . $src . '" alt="' . $title . '" />';
-        echo '</div>';
-        echo '</body>';
-        echo '</html>';
+	echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-Strict.dtd">';
+	echo '<html>';
+	echo '<head>';
+	echo '<meta http-equiv="content-type" content="text/html; charset=UTF-8" />';
+	echo '<title>' . $title . '</title>';
+	echo '</head>';
+	echo '<body>';
+	echo '<div>';
+	echo '<img src="' . $src . '" alt="' . $title . '" />';
+	echo '</div>';
+	echo '</body>';
+	echo '</html>';
 }
 
 /**
@@ -167,103 +167,114 @@ function wrap_img_in_html($src, $title)
 */
 function send_file_to_browser($attachment, $upload_dir, $category)
 {
-        global $user, $db, $config, $phpbb_root_path;
+	global $user, $db, $config, $phpbb_root_path;
 
-        $filename = $phpbb_root_path . $upload_dir . '/' . $attachment['physical_filename'];
+	$filename = $phpbb_root_path . $upload_dir . '/' . $attachment['physical_filename'];
 
-        if (!@file_exists($filename))
-        {
-                trigger_error($user->lang['ERROR_NO_ATTACHMENT'] . '<br /><br />' . sprintf($user->lang['FILE_NOT_FOUND_404'], $filename));
-        }
+	if (!@file_exists($filename))
+	{
+		trigger_error($user->lang['ERROR_NO_ATTACHMENT'] . '<br /><br />' . sprintf($user->lang['FILE_NOT_FOUND_404'], $filename));
+	}
 
-        // Correct the mime type - we force application/octetstream for all files, except images
-        // Please do not change this, it is a security precaution
-        if ($category != ATTACHMENT_CATEGORY_IMAGE || strpos($attachment['mimetype'], 'image') !== 0)
-        {
-                $attachment['mimetype'] = (strpos(strtolower($user->browser), 'msie') !== false || strpos(strtolower($user->browser), 'opera') !== false) ? 'application/octetstream' : 'application/octet-stream';
-        }
+	// Correct the mime type - we force application/octetstream for all files, except images
+	// Please do not change this, it is a security precaution
+	if ($category != ATTACHMENT_CATEGORY_IMAGE || strpos($attachment['mimetype'], 'image') !== 0)
+	{
+		$attachment['mimetype'] = (strpos(strtolower($user->browser), 'msie') !== false || strpos(strtolower($user->browser), 'opera') !== false) ? 'application/octetstream' : 'application/octet-stream';
+	}
 
-        if (@ob_get_length())
-        {
-                @ob_end_clean();
-        }
+	if (@ob_get_length())
+	{
+		@ob_end_clean();
+	}
 
-        // Now send the File Contents to the Browser
-        $size = @filesize($filename);
+	// Now send the File Contents to the Browser
+	$size = @filesize($filename);
 
-        // To correctly display further errors we need to make sure we are using the correct headers for both (unsetting content-length may not work)
+	// To correctly display further errors we need to make sure we are using the correct headers for both (unsetting content-length may not work)
 
-        // Check if headers already sent or not able to get the file contents.
-        if (headers_sent() || !@file_exists($filename) || !@is_readable($filename))
-        {
-                // PHP track_errors setting On?
-                if (!empty($php_errormsg))
-                {
-                        trigger_error($user->lang['UNABLE_TO_DELIVER_FILE'] . '<br />' . sprintf($user->lang['TRACKED_PHP_ERROR'], $php_errormsg));
-                }
+	// Check if headers already sent or not able to get the file contents.
+	if (headers_sent() || !@file_exists($filename) || !@is_readable($filename))
+	{
+		// PHP track_errors setting On?
+		if (!empty($php_errormsg))
+		{
+			trigger_error($user->lang['UNABLE_TO_DELIVER_FILE'] . '<br />' . sprintf($user->lang['TRACKED_PHP_ERROR'], $php_errormsg));
+		}
 
-                trigger_error('UNABLE_TO_DELIVER_FILE');
-        }
+		trigger_error('UNABLE_TO_DELIVER_FILE');
+	}
 
-        // Now the tricky part... let's dance
-        header('Pragma: public');
+	// Now the tricky part... let's dance
+	header('Pragma: public');
 
-        /**
-        * Commented out X-Sendfile support. To not expose the physical filename within the header if xsendfile is absent we need to look into methods of checking it's status.
-        *
-        * Try X-Sendfile since it is much more server friendly - only works if the path is *not* outside of the root path...
-        * lighttpd has core support for it. An apache2 module is available at http://celebnamer.celebworld.ws/stuff/mod_xsendfile/
-        *
-        * Not really ideal, but should work fine...
-        * <code>
-        *        if (strpos($upload_dir, '/') !== 0 && strpos($upload_dir, '../') === false)
-        *        {
-        *                header('X-Sendfile: ' . $filename);
-        *        }
-        * </code>
-        */
+	/**
+	* Commented out X-Sendfile support. To not expose the physical filename within the header if xsendfile is absent we need to look into methods of checking it's status.
+	*
+	* Try X-Sendfile since it is much more server friendly - only works if the path is *not* outside of the root path...
+	* lighttpd has core support for it. An apache2 module is available at http://celebnamer.celebworld.ws/stuff/mod_xsendfile/
+	*
+	* Not really ideal, but should work fine...
+	* <code>
+	*	if (strpos($upload_dir, '/') !== 0 && strpos($upload_dir, '../') === false)
+	*	{
+	*		header('X-Sendfile: ' . $filename);
+	*	}
+	* </code>
+	*/
 
-        // Send out the Headers. Do not set Content-Disposition to inline please, it is a security measure for users using the Internet Explorer.
-        header('Content-Type: ' . $attachment['mimetype']);
+	// Send out the Headers. Do not set Content-Disposition to inline please, it is a security measure for users using the Internet Explorer.
+	$is_ie8 = (strpos(strtolower($user->browser), 'msie 8.0') !== false);
+	header('Content-Type: ' . $attachment['mimetype'] . (($is_ie8) ? '; authoritative=true;' : ''));
 
-        if (empty($user->browser) || (strpos(strtolower($user->browser), 'msie') !== false))
-        {
-                header('Content-Disposition: attachment; ' . header_filename(htmlspecialchars_decode($attachment['real_filename'])));
-                if (empty($user->browser) || (strpos(strtolower($user->browser), 'msie 6.0') !== false))
-                {
-                        header('expires: -1');
-                }
-        }
-        else
-        {
-                header('Content-Disposition: ' . ((strpos($attachment['mimetype'], 'image') === 0) ? 'inline' : 'attachment') . '; ' . header_filename(htmlspecialchars_decode($attachment['real_filename'])));
-        }
+	if (empty($user->browser) || (!$is_ie8 && (strpos(strtolower($user->browser), 'msie') !== false)))
+	{
+		header('Content-Disposition: attachment; ' . header_filename(htmlspecialchars_decode($attachment['real_filename'])));
+		if (empty($user->browser) || (strpos(strtolower($user->browser), 'msie 6.0') !== false))
+		{
+			header('expires: -1');
+		}
+	}
+	else
+	{
+		header('Content-Disposition: ' . ((strpos($attachment['mimetype'], 'image') === 0) ? 'inline' : 'attachment') . '; ' . header_filename(htmlspecialchars_decode($attachment['real_filename'])));
+		if ($is_ie8 && (strpos($attachment['mimetype'], 'image') !== 0))
+		{
+			header('X-Download-Options: noopen');
+		}
+	}
 
-        if ($size)
-        {
-                header("Content-Length: $size");
-        }
+	if ($size)
+	{
+		header("Content-Length: $size");
+	}
 
-        // Try to deliver in chunks
-        @set_time_limit(0);
+	// Close the db connection before sending the file
+	$db->sql_close();
 
-        $fp = @fopen($filename, 'rb');
+	if (!set_modified_headers($attachment['filetime'], $user->browser))
+	{
+		// Try to deliver in chunks
+		@set_time_limit(0);
 
-        if ($fp !== false)
-        {
-                while (!feof($fp))
-                {
-                        echo fread($fp, 8192);
-                }
-                fclose($fp);
-        }
-        else
-        {
-                @readfile($filename);
-        }
+		$fp = @fopen($filename, 'rb');
 
-        flush();
-        exit;
+		if ($fp !== false)
+		{
+			while (!feof($fp))
+			{
+				echo fread($fp, 8192);
+			}
+			fclose($fp);
+		}
+		else
+		{
+			@readfile($filename);
+		}
+
+		flush();
+	}
+	file_gc();
 }
 
 /**
@@ -271,17 +282,17 @@ function send_file_to_browser($attachment, $upload_dir, $category)
 */
 function header_filename($file)
 {
-        $user_agent = (!empty($_SERVER['HTTP_USER_AGENT'])) ? htmlspecialchars((string) $_SERVER['HTTP_USER_AGENT']) : '';
+	$user_agent = (!empty($_SERVER['HTTP_USER_AGENT'])) ? htmlspecialchars((string) $_SERVER['HTTP_USER_AGENT']) : '';
 
-        // There be dragons here.
-        // Not many follows the RFC...
-        if (strpos($user_agent, 'MSIE') !== false || strpos($user_agent, 'Safari') !== false || strpos($user_agent, 'Konqueror') !== false)
-        {
-                return "filename=" . rawurlencode($file);
-        }
+	// There be dragons here.
+	// Not many follows the RFC...
+	if (strpos($user_agent, 'MSIE') !== false || strpos($user_agent, 'Safari') !== false || strpos($user_agent, 'Konqueror') !== false)
+	{
+		return "filename=" . rawurlencode($file);
+	}
 
-        // follow the RFC for extended filename for the rest
-        return "filename*=UTF-8''" . rawurlencode($file);
+	// follow the RFC for extended filename for the rest
+	return "filename*=UTF-8''" . rawurlencode($file);
 }
 
 /**
@@ -289,110 +300,153 @@ function header_filename($file)
 */
 function download_allowed()
 {
-        global $config, $user, $db;
+	global $config, $user, $db;
 
-        if (!$config['secure_downloads'])
-        {
-                return true;
-        }
+	if (!$config['secure_downloads'])
+	{
+		return true;
+	}
 
-        $url = (!empty($_SERVER['HTTP_REFERER'])) ? trim($_SERVER['HTTP_REFERER']) : trim(getenv('HTTP_REFERER'));
+	$url = (!empty($_SERVER['HTTP_REFERER'])) ? trim($_SERVER['HTTP_REFERER']) : trim(getenv('HTTP_REFERER'));
 
-        if (!$url)
-        {
-                return ($config['secure_allow_empty_referer']) ? true : false;
-        }
+	if (!$url)
+	{
+		return ($config['secure_allow_empty_referer']) ? true : false;
+	}
 
-        // Split URL into domain and script part
-        $url = @parse_url($url);
+	// Split URL into domain and script part
+	$url = @parse_url($url);
 
-        if ($url === false)
-        {
-                return ($config['secure_allow_empty_referer']) ? true : false;
-        }
+	if ($url === false)
+	{
+		return ($config['secure_allow_empty_referer']) ? true : false;
+	}
 
-        $hostname = $url['host'];
-        unset($url);
+	$hostname = $url['host'];
+	unset($url);
 
-        $allowed = ($config['secure_allow_deny']) ? false : true;
-        $iplist = array();
+	$allowed = ($config['secure_allow_deny']) ? false : true;
+	$iplist = array();
 
-        if (($ip_ary = @gethostbynamel($hostname)) !== false)
-        {
-                foreach ($ip_ary as $ip)
-                {
-                        if ($ip)
-                        {
-                                $iplist[] = $ip;
-                        }
-                }
-        }
+	if (($ip_ary = @gethostbynamel($hostname)) !== false)
+	{
+		foreach ($ip_ary as $ip)
+		{
+			if ($ip)
+			{
+				$iplist[] = $ip;
+			}
+		}
+	}
 
-        // Check for own server...
-        $server_name = (!empty($_SERVER['SERVER_NAME'])) ? $_SERVER['SERVER_NAME'] : getenv('SERVER_NAME');
+	// Check for own server...
+	$server_name = $user->host;
 
-        // Forcing server vars is the only way to specify/override the protocol
-        if ($config['force_server_vars'] || !$server_name)
-        {
-                $server_name = $config['server_name'];
-        }
+	// Forcing server vars is the only way to specify/override the protocol
+	if ($config['force_server_vars'] || !$server_name)
+	{
+		$server_name = $config['server_name'];
+	}
 
-        if (preg_match('#^.*?' . preg_quote($server_name, '#') . '.*?$#i', $hostname))
-        {
-                $allowed = true;
-        }
+	if (preg_match('#^.*?' . preg_quote($server_name, '#') . '.*?$#i', $hostname))
+	{
+		$allowed = true;
+	}
 
-        // Get IP's and Hostnames
-        if (!$allowed)
-        {
-                $sql = 'SELECT site_ip, site_hostname, ip_exclude
-                        FROM ' . SITELIST_TABLE;
-                $result = $db->sql_query($sql);
+	// Get IP's and Hostnames
+	if (!$allowed)
+	{
+		$sql = 'SELECT site_ip, site_hostname, ip_exclude
+			FROM ' . SITELIST_TABLE;
+		$result = $db->sql_query($sql);
 
-                while ($row = $db->sql_fetchrow($result))
-                {
-                        $site_ip = trim($row['site_ip']);
-                        $site_hostname = trim($row['site_hostname']);
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$site_ip = trim($row['site_ip']);
+			$site_hostname = trim($row['site_hostname']);
 
-                        if ($site_ip)
-                        {
-                                foreach ($iplist as $ip)
-                                {
-                                        if (preg_match('#^' . str_replace('\*', '.*?', preg_quote($site_ip, '#')) . '$#i', $ip))
-                                        {
-                                                if ($row['ip_exclude'])
-                                                {
-                                                        $allowed = ($config['secure_allow_deny']) ? false : true;
-                                                        break 2;
-                                                }
-                                                else
-                                                {
-                                                        $allowed = ($config['secure_allow_deny']) ? true : false;
-                                                }
-                                        }
-                                }
-                        }
+			if ($site_ip)
+			{
+				foreach ($iplist as $ip)
+				{
+					if (preg_match('#^' . str_replace('\*', '.*?', preg_quote($site_ip, '#')) . '$#i', $ip))
+					{
+						if ($row['ip_exclude'])
+						{
+							$allowed = ($config['secure_allow_deny']) ? false : true;
+							break 2;
+						}
+						else
+						{
+							$allowed = ($config['secure_allow_deny']) ? true : false;
+						}
+					}
+				}
+			}
 
-                        if ($site_hostname)
-                        {
-                                if (preg_match('#^' . str_replace('\*', '.*?', preg_quote($site_hostname, '#')) . '$#i', $hostname))
-                                {
-                                        if ($row['ip_exclude'])
-                                        {
-                                                $allowed = ($config['secure_allow_deny']) ? false : true;
-                                                break;
-                                        }
-                                        else
-                                        {
-                                                $allowed = ($config['secure_allow_deny']) ? true : false;
-                                        }
-                                }
-                        }
-                }
-                $db->sql_freeresult($result);
-        }
+			if ($site_hostname)
+			{
+				if (preg_match('#^' . str_replace('\*', '.*?', preg_quote($site_hostname, '#')) . '$#i', $hostname))
+				{
+					if ($row['ip_exclude'])
+					{
+						$allowed = ($config['secure_allow_deny']) ? false : true;
+						break;
+					}
+					else
+					{
+						$allowed = ($config['secure_allow_deny']) ? true : false;
+					}
+				}
+			}
+		}
+		$db->sql_freeresult($result);
+	}
 
-        return $allowed;
+	return $allowed;
 }
 
+/**
+* Check if the browser has the file already and set the appropriate headers-
+* @returns false if a resend is in order.
+*/
+function set_modified_headers($stamp, $browser)
+{
+	// let's see if we have to send the file at all
+	$last_load 	=  isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? strtotime(trim($_SERVER['HTTP_IF_MODIFIED_SINCE'])) : false;
+	if ((strpos(strtolower($browser), 'msie 6.0') === false) && (strpos(strtolower($browser), 'msie 8.0') === false))
+	{
+		if ($last_load !== false && $last_load <= $stamp)
+		{
+			if (@php_sapi_name() === 'CGI')
+			{
+				header('Status: 304 Not Modified', true, 304);
+			}
+			else
+			{
+				header('HTTP/1.0 304 Not Modified', true, 304);
+			}
+			// seems that we need those too ... browsers
+			header('Pragma: public');
+			header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + 31536000));
+			return true;
+		}
+		else
+		{
+			header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $stamp) . ' GMT');
+		}
+	}
+	return false;
+}
+
+function file_gc()
+{
+	global $cache, $db;
+	if (!empty($cache))
+	{
+		$cache->unload();
+	}
+	$db->sql_close();
+	exit;
+}
 ?>
